5255989e1bed3e1fb8e97eedd26b19f49a476721
Jay Depala <jay.depala@valuelabs.com>
2023-03-14 16:19:39 +0530

    Add webpack prod

---
+++ home/webpack.prod.js
@@ -0,0 +1,81 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const webpack = require('webpack');
+const path = require('path');
+// const DashboardPlugin = require('webpack-dashboard/plugin');
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    publicPath: "http://app-mfe-react.s3-website-us-east-1.amazonaws.com/home/"
+  },
+
+  resolve: {
+    modules: [
+      path.resolve(__dirname, 'src'),
+      'node_modules'
+    ],
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3001,
+    historyApiFallback: true
+  },
+
+  module: {
+    rules: [
+      // {
+      //   test: /\.m?js/,
+      //   type: "javascript/auto",
+      //   resolve: {
+      //     fullySpecified: false
+      //   },
+      // },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"]
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "home",
+      filename: "remoteEntry.js",
+      remotes: {
+        'home': 'home@http://app-mfe-react.s3-website-us-east-1.amazonaws.com/home/remoteEntry.js',
+        'settings': 'settings@http://app-mfe-react.s3-website-us-east-1.amazonaws.com/settings/remoteEntry.js',
+        'users': 'users@http://app-mfe-react.s3-website-us-east-1.amazonaws.com/users/remoteEntry.js',
+        'data': 'data@http://app-mfe-react.s3-website-us-east-1.amazonaws.com/data/remoteEntry.js'
+      },
+      exposes: {
+        './Navbar': './src/Navbar/Navbar.tsx',
+        './useLongPress': './src/Utils/useLongPress.tsx',
+        './ApiService': './src/Utils/Api.Service.tsx'
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"]
+        }
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html"
+    }),
+    // new DashboardPlugin(),
+  ],
+};
---
+++ settings/webpack.prod.js
@@ -0,0 +1,72 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+module.exports = {
+  output: {
+    publicPath: "http://app-mfe-react.s3-website-us-east-1.amazonaws.com/settings/",
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3002,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      // {
+      //   test: /\.m?js/,
+      //   type: "javascript/auto",
+      //   resolve: {
+      //     fullySpecified: false,
+      //   },
+      // },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "settings",
+      filename: "remoteEntry.js",
+      remotes: {
+        'home': 'home@http://app-mfe-react.s3-website-us-east-1.amazonaws.com/home/remoteEntry.js',
+        'settings': 'settings@http://app-mfe-react.s3-website-us-east-1.amazonaws.com/settings/remoteEntry.js',
+        'users': 'users@http://app-mfe-react.s3-website-us-east-1.amazonaws.com/users/remoteEntry.js',
+        'data': 'data@http://app-mfe-react.s3-website-us-east-1.amazonaws.com/data/remoteEntry.js'
+      },
+      exposes: {
+        './SettingsList': './src/Settings/SettingsList.tsx',
+        './SettingsService': './src/Settings/Settings.service.tsx'
+      },
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
